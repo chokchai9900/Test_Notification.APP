6e5e37c6f36c7db5a3e7591248dbca3e1ec92850
chokchai9900 <chokchai9900@gmail.com>
2020-02-21 09:03:53 +0700

    Add Handle Noti

--- Test_Notification.APP/Test_Notification.APP/App.xaml.cs
+++ Test_Notification.APP/Test_Notification.APP/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Com.OneSignal;
 using Com.OneSignal.Abstractions;
 using System;
+using System.Diagnostics;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,7 +16,9 @@ namespace Test_Notification.APP
             MainPage = new MainPage();
             OneSignal.Current.StartInit("d852feb3-ef79-403f-b8cd-ceddc02f4654")
                 .HandleNotificationReceived(HandleNotificationReceived)
+                .InFocusDisplaying(OSInFocusDisplayOption.None)
                 .EndInit();
+
 
             GetPlayerId();
 
@@ -28,7 +31,13 @@ namespace Test_Notification.APP
 
         private async void HandleNotificationReceived(OSNotification notification)
         {
-            await MainPage.DisplayAlert("Additional Data ", notification.payload.additionalData["endpoint"].ToString(), "Ok");
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await MainPage.DisplayAlert("Additional Data 1", notification.payload.additionalData["endpoint1"].ToString(), "Ok");
+                await MainPage.DisplayAlert("Additional Data 2", notification.payload.additionalData["endpoint2"].ToString(), "Ok");
+            });
+            //Debug.WriteLine("##### Noti ######");
+            //Debug.WriteLine(notification.payload.additionalData["endpoint"].ToString());
         }
 
         protected override void OnStart()
